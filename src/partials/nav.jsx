f7bd2c94b596774123b79f6ee12e6af2ccc98f66
Jerry Vargas <jvargas@conversionia.com>
2020-11-04 16:39:00 -0600

    add

--- src/partials/nav.jsx
+++ src/partials/nav.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import logoImg from '../assets/images/logo.png';
 
 const nav = () => {
-    return (
-        <nav id="nav" className="flex items-center justify-between py-5">
-           <img src={logoImg} alt="Jerry Vargas Logo" className="h-24 p-4 bg-black"/>
-           <div className="flex">
-               <a className="nav-btn" href="#">Lorem</a>
-               <a className="nav-btn" href="#">Lorem</a>
-               <a className="nav-btn" href="#">Lorem</a>
-               <a className="nav-btn" href="#">Lorem</a>
-               <a className="nav-btn" href="#">Lorem</a>
-           </div>
-        </nav>
-    )
+return (
+<nav id="nav" className="flex items-center justify-between py-5">
+    <img src={logoImg} alt="Jerry Vargas Logo" className="h-24 p-4 bg-black" />
+    <div className="flex xs:hidden sm:hidden md:block">
+        <a className="nav-btn" href="#">Lorem</a>
+        <a className="nav-btn" href="#">Lorem</a>
+        <a className="nav-btn" href="#">Lorem</a>
+        <a className="nav-btn" href="#">Lorem</a>
+        <a className="nav-btn" href="#">Lorem</a>
+    </div>    
+    <button className="xs:block sm:block md:hidden">Juice</button>
+</nav>
+)
 }
 
 export default nav
