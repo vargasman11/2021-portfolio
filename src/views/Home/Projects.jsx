211fbb17294ae3c515bde82a0d9c8ff2d30e5c30
Jerry Vargas <vargasman11@gmail.com>
2020-11-27 18:33:25 -0600

    add

--- src/views/Home/Projects.jsx
+++ src/views/Home/Projects.jsx
@@ -5,6 +5,8 @@ import hornady from '../../assets/images/projects/hornady-md.png';
 import pendulux from '../../assets/images/projects/pendulux-md.png';
 import averitt from '../../assets/images/projects/mjml-averitt-md-v2.png';
 import corgi from '../../assets/images/projects/corgi-md.png';
+import altmann from '../../assets/images/projects/altmann-md.png';
+import usatruck from '../../assets/images/projects/usatruck-md.png';
 
 function Projects() {
 return (
@@ -18,14 +20,14 @@ return (
         <div className="row">
             <div className="col-sm-12">
                 <div id="gridProjects">
-                    <div className="grid-item grid-item-1"><img  src={lfmax} alt="" /></div>
-                    <div className="grid-item grid-item-2"><img  src={hornady} alt="" /></div>
-                    <div className="grid-item grid-item-3"><img  src={pendulux} alt="" /></div>
-                    <div className="grid-item grid-item-4"><img  src={averitt} alt="" /></div>
-                    <div className="grid-item grid-item-5"><img  src={corgi} alt="" /></div>
-                    <div className="grid-item grid-item-6"><img  src={ee} alt="" /></div>
-                    <div className="grid-item grid-item-7"><img  src={ee} alt="" /></div>
-                    <div className="grid-item grid-item-8"><img  src={ee} alt="" /></div>
+                    <div className="grid-item grid-item-1"></div>
+                    <div className="grid-item grid-item-2"></div>
+                    <div className="grid-item grid-item-3"></div>
+                    <div className="grid-item grid-item-4"></div>
+                    <div className="grid-item grid-item-5"></div>
+                    <div className="grid-item grid-item-6"></div>
+                    <div className="grid-item grid-item-7"></div>
+                    <div className="grid-item grid-item-8"></div>
                 </div>
             </div>
         </div>
